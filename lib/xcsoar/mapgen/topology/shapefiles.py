85f9a6213401663319e6992803f04abc3bdd7c81
Daniel Mendler <mail@daniel-mendler.de>
2011-02-27 21:05:57 +0100

    download topology layer description from download server
    * this allows data updates which change the layer names etc
    * http://download.xcsoar.org/mapgen/data/topology.json

--- lib/xcsoar/mapgen/topology/shapefiles.py
+++ lib/xcsoar/mapgen/topology/shapefiles.py
@@ -1,92 +1,18 @@
 import os
 import subprocess
+import json
 
+from xcsoar.mapgen.util import slurp
 from xcsoar.mapgen.georect import GeoRect
 from xcsoar.mapgen.filelist import FileList
 
 __cmd_ogr2ogr = 'ogr2ogr'
 __cmd_shptree = 'shptree'
 
-__vmap0_datasets = [
-     { 'name': 'vmap0/eur', 'bounds': GeoRect( -35, 180, 90,  30) },
-     { 'name': 'vmap0/noa', 'bounds': GeoRect(-180,   0, 90, -40) },
-     { 'name': 'vmap0/sas', 'bounds': GeoRect(  25, 180, 50, -60) },
-     { 'name': 'vmap0/soa', 'bounds': GeoRect(-180, 180, 30, -90) },
-     ]
-
-__osm_datasets = [
-     { 'name': 'osm/planet', 'bounds': GeoRect(-180, 180, 90, -90) },
-     ]
-
-__layers = [
-     { 'name':     'city_area',
-       'datasets': __vmap0_datasets,
-       'layer':    'pop-built-up-a',
-       'range':    50,
-       'color':    '223,223,0' },
-     { 'name':     'water_area',
-       'datasets': __vmap0_datasets,
-       'layer':    'hydro-inland-water-a',
-       'label':    'nam',
-       'where':    'hyc=8',
-       'range':    100,
-       'color':    '85,160,255' },
-     { 'name':     'water_line',
-       'datasets': __vmap0_datasets,
-       'layer':    'hydro-water-course-l',
-       'label':    'nam',
-       'where':    'hyc=8',
-       'range':    50,
-       'color':    '85,160,255' },
-     { 'name':     'roadbig_line',
-       'datasets': __osm_datasets,
-       'layer':    'roadbig_line',
-       'range':    15,
-       'color':    '240,64,64',
-       'pen_width': 3 },
-     { 'name':     'roadmedium_line',
-       'datasets': __osm_datasets,
-       'layer':    'roadmedium_line',
-       'range':    8,
-       'color':    '240,64,64',
-       'pen_width': 2 },
-     { 'name':     'roadsmall_line',
-       'datasets': __osm_datasets,
-       'layer':    'roadsmall_line',
-       'range':    3,
-       'color':    '240,64,64' },
-     { 'name':     'railway_line',
-       'datasets': __vmap0_datasets,
-       'layer':    'trans-railroad-l',
-       'range':    10,
-       'where':    'exs=28',
-       'color':    '64,64,64'   },
-     { 'name':     'citybig_point',
-       'datasets': __osm_datasets,
-       'layer':    'citybig_point',
-       'label':    'name',
-       'range':    15,
-       'label_important_range': 10,
-       'color':    '223,223,0'  },
-     { 'name':     'citymedium_point',
-       'datasets': __osm_datasets,
-       'layer':    'citymedium_point',
-       'label':    'name',
-       'range':    10,
-       'label_important_range': 3,
-       'color':    '223,223,0'  },
-     { 'name':     'citysmall_point',
-       'datasets': __osm_datasets,
-       'layer':    'citysmall_point',
-       'label':    'name',
-       'range':    3,
-       'color':    '223,223,0'  },
-     ]
-
 def __filter_datasets(bounds, datasets):
      filtered = []
      for dataset in datasets:
-         if bounds.intersects(dataset['bounds']):
+         if bounds.intersects(GeoRect(*dataset['bounds'])):
              filtered.append(dataset)
      return filtered
 
@@ -103,10 +29,10 @@ def __create_layer_from_dataset(bounds, layer, dataset, append, downloader, dir_
          arg.append("-update")
          arg.append("-append")
 
-    if 'where' in layer:
-         arg.extend(["-where", layer['where']])
+    if 'src_where' in layer:
+         arg.extend(["-where", layer['src_where']])
 
-    arg.extend(["-select", layer['label'] if 'label' in layer else ''])
+    arg.extend(["-select", layer['src_label'] if 'src_label' in layer else ''])
 
     arg.extend(["-spat",
                 str(bounds.left),
@@ -117,7 +43,7 @@ def __create_layer_from_dataset(bounds, layer, dataset, append, downloader, dir_
     arg.append(dir_temp)
     arg.append(data_dir)
 
-    arg.append(layer['layer'])
+    arg.append(layer['src_layer'])
     arg.extend(['-nln', layer['name']])
 
     subprocess.check_call(arg)
@@ -126,10 +52,10 @@ def __create_layer_index(layer, dir_temp):
     print('Generating index file for layer {} ...'.format(layer['name']))
     subprocess.check_call([__cmd_shptree, os.path.join(dir_temp, layer['name'] + ".shp")])
 
-def __create_layer(bounds, layer, downloader, dir_temp, files, index):
+def __create_layer(bounds, layer, datasets, downloader, dir_temp, files, index):
     print('Creating topology layer {} ...'.format(layer['name']))
 
-    datasets = __filter_datasets(bounds, layer['datasets'])
+    datasets = __filter_datasets(bounds, datasets)
     for i in range(len(datasets)):
         __create_layer_from_dataset(bounds, layer, datasets[i],
                                     i != 0, downloader, dir_temp)
@@ -151,7 +77,7 @@ def __create_index_file(dir_temp, index):
          for layer in index:
               file.write(layer['name'] + ',' +
                          str(layer['range']) + ',,' +
-                         ('1' if 'label' in layer else '') + ',' +
+                         ('1' if 'src_label' in layer else '') + ',' +
                          layer['color'] + ',' +
                          str(layer.get('pen_width', 1)) + ',' +
                          str(layer.get('label_range', layer['range'])) + ',' +
@@ -161,10 +87,12 @@ def __create_index_file(dir_temp, index):
     return os.path.join(dir_temp, "topology.tpl")
 
 def create(bounds, downloader, dir_temp):
-    files = FileList()
-    index = []
-    for layer in __layers:
-        __create_layer(bounds, layer, downloader, dir_temp, files, index)
+     data = json.loads(slurp(downloader.retrieve('topology.json')))
 
-    files.add(__create_index_file(dir_temp, index), True)
-    return files
+     files = FileList()
+     index = []
+     for layer in data['layers']:
+          __create_layer(bounds, layer, data['datasets'][layer['src_dataset']], downloader, dir_temp, files, index)
+
+     files.add(__create_index_file(dir_temp, index), True)
+     return files
