5673a22259626ef086fdcbf815a29e4fd04f30cb
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-08-25 23:38:40 +0200

    waypoint_list: Moved WinPilot DAT file parser to seperate file

--- lib/xcsoar/mapgen/waypoints/waypoint_list.py
+++ lib/xcsoar/mapgen/waypoints/waypoint_list.py
@@ -1,5 +1,6 @@
 from xcsoar.mapgen.waypoints.waypoint import Waypoint
 from xcsoar.mapgen.waypoints.seeyou import parse_seeyou_waypoints
+from xcsoar.mapgen.waypoints.winpilot import parse_winpilot_waypoints
 from xcsoar.mapgen.georect import GeoRect
 from xcsoar.mapgen.geopoint import GeoPoint
 
@@ -44,50 +45,10 @@ class WaypointList:
 
     def parse(self, lines, filename = 'unknown.dat'):
         if filename.lower().endswith('.xcw') or filename.lower().endswith('.dat'):
-            self.__parse_winpilot(lines)
+            self.extend(parse_winpilot_waypoints(lines))
         elif filename.lower().endswith('.cup'):
             self.extend(parse_seeyou_waypoints(lines))
         else:
             raise RuntimeError('Waypoint file {} has an unsupported format.'.format(filename))
+        
         return self
-    
-    def __parse_winpilot(self, lines):
-        for line in lines:
-            line = line.strip()
-            if line == '' or line.startswith('*'):
-                continue
-
-            fields = line.split(',')
-            if len(fields) < 6:
-                continue
-
-            wp = Waypoint()
-            wp.lat = self.__parse_winpilot_coordinate(fields[1]);
-            wp.lon = self.__parse_winpilot_coordinate(fields[2]);
-            wp.altitude = self.__parse_winpilot_altitude(fields[3]);
-            wp.name = fields[5].strip();
-            self.append(wp)
-
-    def __parse_winpilot_altitude(self, str):
-        str = str.lower()
-        if str.endswith('ft') or str.endswith('f'):
-            str = str.rstrip('ft')
-            return int(str) * 0.3048
-        else:
-            str = str.rstrip('m')
-            return int(str)
-
-    def __parse_winpilot_coordinate(self, str):
-        str = str.lower()
-        negative = str.endswith('s') or str.endswith('w')
-        str = str.rstrip('sw') if negative else str.rstrip('ne')
-
-        str = str.split(':')
-        if len(str) < 2:
-            return None
-
-        # degrees + minutes / 60
-        a = int(str[0]) + float(str[1]) / 60
-        if (negative):
-            a *= -1
-        return a
