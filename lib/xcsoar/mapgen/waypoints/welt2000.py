3cceed0707bc276200dafe5de45a57ed96cd7e5a
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-08-27 22:57:49 +0200

    welt2000: Download data directly from the source server

--- lib/xcsoar/mapgen/waypoints/welt2000.py
+++ lib/xcsoar/mapgen/waypoints/welt2000.py
@@ -1,15 +1,22 @@
 import os
+import subprocess
 
 from xcsoar.mapgen.waypoints.welt2000_reader import parse_welt2000_waypoints
 from xcsoar.mapgen.waypoints.seeyou_writer import write_seeyou_waypoints
 from xcsoar.mapgen.filelist import FileList
 
-def __get_database_file(downloader, dir_data):
+def __get_database_file(dir_data):
     path = os.path.join(dir_data, 'welt2000', 'WELT2000.TXT')
 
-    if not os.path.exists(path):
-        downloader.retrieve_extracted('welt2000/WELT2000.7z')
-        
+    # Create Welt2000 data folder if necessary
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    
+    # Download the current file
+    # (only if server file is newer than local file)
+    url = 'http://www.segelflug.de/vereine/welt2000/download/WELT2000.TXT'
+    subprocess.check_call(['wget', '-N', '-P', os.path.dirname(path), url])
+
     # Check if download succeeded
     if not os.path.exists(path):        
         raise RuntimeError('Welt2000 database not found at {}'.format(path))
@@ -17,9 +24,9 @@ def __get_database_file(downloader, dir_data):
     # Return path to the Welt2000 file
     return path
 
-def get_database(downloader, dir_data, bounds = None):
+def get_database(dir_data, bounds = None):
     # Get Welt2000 file
-    path = __get_database_file(downloader, dir_data)
+    path = __get_database_file(dir_data)
     
     # Parse Welt2000 file
     with open(path, "r") as f:
@@ -34,8 +41,8 @@ def __create_waypoint_file(database, dir_temp):
     write_seeyou_waypoints(database, path)
     return path
 
-def create(downloader, dir_data, dir_temp, bounds = None):
-    database = get_database(downloader, dir_data, bounds)
+def create(dir_data, dir_temp, bounds = None):
+    database = get_database(dir_data, bounds)
     file = __create_waypoint_file(database, dir_temp)
     
     list = FileList()
