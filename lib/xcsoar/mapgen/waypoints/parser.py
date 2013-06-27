ca2561f2c1556c89700ccf1782eebd88da17b9b9
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-06-27 20:17:29 +0200

    waypoints/parser: Fixed duplicate parse_waypoint_file() function bug

--- lib/xcsoar/mapgen/waypoints/parser.py
+++ lib/xcsoar/mapgen/waypoints/parser.py
@@ -1,15 +1,11 @@
 from xcsoar.mapgen.waypoints.seeyou_reader import parse_seeyou_waypoints
 from xcsoar.mapgen.waypoints.winpilot_reader import parse_winpilot_waypoints
 
-def parse_waypoint_file(filename):
-    file = open(filename, 'r')
 
-    try:
-        return parse_waypoint_file(filename, file)
-    finally:
-        file.close()
+def parse_waypoint_file(filename, file=None):
+    if not file:
+        file = open(filename, 'r')
 
-def parse_waypoint_file(filename, file):
     if filename.lower().endswith('.xcw') or filename.lower().endswith('.dat'):
         return parse_winpilot_waypoints(file)
     elif filename.lower().endswith('.cup'):
